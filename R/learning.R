8c5b8ec0c121a0a564991ef2df4b90178175a610
Hossein Hazrati <hossein.hazrati@agro.au.dk>
2023-04-25 14:47:27 +0200

    commiting the readme

---
+++ R/learning.R
@@ -0,0 +1,15 @@
+# Basics of R -------------------------------------------------------------
+
+weight_kilos <- 100
+colnames(airquality)
+str(airquality)
+summary(airquality)
+1 + 1
+mean(2:6)
+æ
+
+# loading packages --------------------------------------------------------
+
+library(tidyverse)
+r3::check_git_config()
+r3::setup_git_config()
